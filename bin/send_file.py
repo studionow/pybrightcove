c00f73a2fae4bffc11959c7d2ed71b2a61b467d4
Patrick Altman <patrick@studionow.com>
2009-02-04 21:05:02 -0600

    cleaned up some stuff

--- bin/send_file.py
+++ bin/send_file.py
@@ -6,8 +6,8 @@ c = Connection()
 import sys
 
 video = Video()
-video.title = "My Test File"
-video.short_description = "This is a short description"
+video.title = sys.argv[1]
+video.short_description = sys.argv[1]
 
 print "Attempting to upload %s" % sys.argv[1]
 c.create_video(sys.argv[1], video=video)
--- pybrightcove/connection.py
+++ pybrightcove/connection.py
@@ -1,8 +1,8 @@
 import hashlib
 import simplejson
-import pycurl
-import StringIO
+import urllib2
 
+from pybrightcove.multipart import MultipartPostHandler
 from pybrightcove import config, UserAgent
 from pybrightcove.exceptions import BrightcoveClientError, BrightcoveError, BrightcoveGeneralError
 from pybrightcove.video import Video
@@ -28,50 +28,27 @@ class Connection(object):
         elif config.has_option('Connection', 'write_token'):
             self.write_token = config.get('Connection', 'write_token')
             
-    def _post_request2(self, data, file_to_upload):
-        from pybrightcove.multipart import MultipartPostHandler
-        import urllib2, cookielib
-
-        cookies = cookielib.CookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies),
-                                        MultipartPostHandler)
-        params = { "JSONRPC" : simplejson.dumps(data),
-                   "filePath" : open(file_to_upload, "rb") }
+    def _post_file(self, data, file_to_upload):
+        opener = urllib2.build_opener(MultipartPostHandler)
+        params = { "JSONRPC" : simplejson.dumps(data), "filePath" : open(file_to_upload, "rb") }
         r = opener.open("http://api.brightcove.com/services/post", params)
         print r.read()
         
-        
-    def _post_request(self, data, file_to_upload=None):
-        output = StringIO.StringIO()
-        curl = pycurl.Curl()
-        
-        #print "Sending this data:\n", data
-        payload = []
-        if file_to_upload:
-            payload.append(  ( 'filePath',  ( pycurl.FORM_FILE, file_to_upload ) ) )
-        payload.append( ( 'JSONRPC', simplejson.dumps(data).encode('utf-8')) )
-        
-        curl.setopt( pycurl.URL, "http://api.brightcove.com/services/post")
-        curl.setopt( pycurl.HTTPPOST, payload )
-        curl.setopt( pycurl.HTTPHEADER, ["User-Agent: %s" % UserAgent,])
-        curl.setopt( pycurl.WRITEFUNCTION, output.write )
-        curl.setopt( pycurl.VERBOSE, 1) 
-        curl.perform()
-        
-        print output.getvalue()
-
 
     def create_video(self, filename, video=None, do_checksum=True, create_multiple_renditions=True, preserve_source_rendition=True):
         data = {"method":"create_video",
-                "params": {"token": self.write_token}}
-                ## ,   "create_multiple_renditions": create_multiple_renditions,
-                ##     "preserve_source_rendition": preserve_source_rendition
+                "params": {
+                    "token": self.write_token, 
+                    "create_multiple_renditions": create_multiple_renditions,
+                    "preserve_source_rendition": preserve_source_rendition
+                    }
+                }
         if video:
             data['params']['video'] = video.to_dict()
-        #if do_checksum:
-            #m = hashlib.md5()
-            #m.update(open(filename, 'rb').read())
-            #data['params']['file_checksum'] = m.hexdigest()
-        r = self._post_request2(data=data, file_to_upload=filename)
+        if do_checksum:
+            m = hashlib.md5()
+            m.update(open(filename, 'rb').read())
+            data['params']['file_checksum'] = m.hexdigest()
+        r = self._post_file(data=data, file_to_upload=filename)
         return r
 
