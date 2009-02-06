760d1293dadc31735254ba6c177b5f65e56e12d2
Patrick Altman <patrick@studionow.com>
2009-02-06 11:00:57 -0600

    removed size script

--- bin/size.py
+++
@@ -1,9 +0,0 @@
-import simplejson
-from subprocess import PIPE, Popen
-import sys
-
-out = Popen(['avinspect', sys.argv[1], '--json'], stdout=PIPE).communicate()[0] 
-d = simplejson.loads(out)
-for s in d["streams"]:
-    if s["type"] == "Video":
-        print "%sx%s" % (s["width"], s["height"])
