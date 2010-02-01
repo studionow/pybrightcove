48c624c9855dbd8d6ece084eb2510aa34153dcd4
Ron Drongowski <ron.drongowski@zeit.de>
2010-02-01 18:58:00 +0800

    basic setup py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybrightcove',
+    version = '',
+    author='',
+    author_email='',
+    url='git://github.com/studionow/pybrightcove.git',
+    description="""\
+""",
+    packages=find_packages(),
+    namespace_packages = [],
+    include_package_data = True,
+    zip_safe=False,
+    license='',
+    install_requires=[
+        'simplejson',
+        'setuptools'
+        ]
+)
+
